834d8c1db500b91a4dea3236f8882c9b5732b3f6
sudo-m3z1m <nizilplay2004@gmail.com>
2024-07-01 18:53:09 +0300

    Added some shit in project need to learn more

--- app/src/main/java/com/example/practiceapplication/MainActivity.kt
+++ app/src/main/java/com/example/practiceapplication/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practiceapplication.ui.theme.PracticeApplicationTheme
 
@@ -19,11 +25,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface()
+                {
+                    Greeting(name = "Practice")
                 }
             }
         }
@@ -32,16 +36,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize().wrapContentSize()) {
+            Text(text = "Hello $name")
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PracticeApplicationTheme {
-        Greeting("Android")
+        Greeting("Practice", Modifier.fillMaxSize())
     }
 }
