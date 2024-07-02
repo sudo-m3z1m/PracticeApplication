26f040e851891e260b3bfeaab103ee1f4a0d6e0d
sudo-m3z1m <nizilplay2004@gmail.com>
2024-07-02 18:25:26 +0300

    Added basics UI. Need to learn kotlin.

--- app/src/main/java/com/example/practiceapplication/MainActivity.kt
+++ app/src/main/java/com/example/practiceapplication/MainActivity.kt
@@ -1,23 +1,48 @@
 package com.example.practiceapplication
 
+import android.content.ClipData.Item
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practiceapplication.ui.theme.PracticeApplicationTheme
+
+class Match(first_team: String = "DEFAULT", second_team: String = "DEFAULT",
+            result: String = "DEFAULT", date: Int = 0)
+{
+    public val first_team: String = first_team
+    public val second_team: String = second_team
+    public val result: String = result
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,24 +52,60 @@ class MainActivity : ComponentActivity() {
             PracticeApplicationTheme {
                 Surface()
                 {
-                    Greeting(name = "Practice")
+//                    list_page_creating(generate_matches())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize().wrapContentSize()) {
-            Text(text = "Hello $name")
-        }
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PracticeApplicationTheme {
-        Greeting("Practice", Modifier.fillMaxSize())
+fun ready_preview()
+{
+    val test_match: Match = Match("Godoters", "Unity guys",
+        "Godoters", 22)
+    var array: Array<Match> = arrayOf(test_match)
+    list_page_creating(array)
+}
+
+@Composable
+fun list_page_creating(items_list: Array<Match>)
+{
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Spacer(modifier = Modifier.height(25.dp))
+        Text(text = "Matches", fontSize = 24.sp)
+        Spacer(Modifier.height(30.dp))
+        LazyColumn(horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            items(items_list.size)
+            {
+                index ->
+                    Surface(color = colorResource(id = R.color.teal_700),
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp).height(50.dp))
+                    {
+                        Row(verticalAlignment = Alignment.CenterVertically)
+                        {
+                            Text(text = items_list[index].first_team, modifier = Modifier.padding(horizontal = 10.dp))
+                            Text(text = items_list[index].second_team, modifier = Modifier.padding(horizontal = 10.dp))
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+                    }
+            }
+        }
     }
+}
+
+fun generate_matches(): Array<Match>
+{
+    var items_list: Array<Match> = emptyArray()
+    for (item_index in (1..5))
+    {
+        val item: Match = Match(first_team = "Godoters!", second_team = "UnityGuys",
+            result = "Godoters", date = item_index)
+        items_list[item_index] = item
+    }
+
+    return items_list
 }
