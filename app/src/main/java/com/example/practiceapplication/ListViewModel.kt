acc1a24af586a9962c5aa81325abbc11f8325df3
sudo-m3zim <nizilplay2004@gmail.com>
2024-07-08 13:52:15 +0300

    Fixed problem with api

--- app/src/main/java/com/example/practiceapplication/ListViewModel.kt
+++ app/src/main/java/com/example/practiceapplication/ListViewModel.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.google.gson.stream.JsonReader
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.Converter
@@ -14,6 +15,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 import retrofit2.http.GET
+import java.io.StringReader
+
+data class MatchItem(
+    val MatchNumber: Int = 0,
+    val RoundNumber: Int = 0,
+    val DateUtc: String = "00000000",
+    val Location: String = "default",
+    val HomeTeam: String = "default",
+    val AwayTeam: String = "default",
+    val HomeTeamScore: Int = 0,
+    val AwayTeamScore: Int = 0
+)
 
 class ListViewModel : ViewModel() {
     val first_test_item: Match = Match(0, "Godoters", "Unity guys",
@@ -21,7 +34,7 @@ class ListViewModel : ViewModel() {
     val second_test_item: Match = Match(1, "Godoters", "Unity guys",
         "Godoters", 25122024)
     var items_list: Array<Match> = arrayOf(first_test_item, second_test_item)
-    var matches_list: List<Match> = emptyList()
+    var matches_list: List<MatchItem> = emptyList()
 
     fun on_list_clicked(clicked_match: Match, nav_controller: NavController)
     {
@@ -35,9 +48,9 @@ class ListViewModel : ViewModel() {
         val matches_api = RetrofitClient.create_retrofit().create(RetrofitClient.api_client::class.java)
         GlobalScope.launch()
         {
+            val json_reader: JsonReader = JsonReader(StringReader(""))
             val result = matches_api.get_matches()
-            result_string = result.body().toString()
-            matches_list.plus(result.body())
+            matches_list = result.body()!!.toList()
         }
         return result_string
     }
@@ -49,7 +62,7 @@ class ListViewModel : ViewModel() {
         interface api_client
         {
             @GET("/feed/json/epl-2023")
-            suspend fun get_matches(): Response<Match>
+            suspend fun get_matches(): Response<List<MatchItem>>
         }
 
         fun create_retrofit(): Retrofit
